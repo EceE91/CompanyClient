e66dc222ebcfbed648d442553e4ed64724a3ebfb
ece <eceercan91@hotmail.com>
2018-11-14 15:48:23 +0300

    Add project files.

---
+++ CompanyClient/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CompanyClient.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Get Public Info" Width="174px" />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Get Confidential Info" />
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Invoke Rest Payment" />
+        <asp:Label ID="Label3" runat="server" Text="Label"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ CompanyClient/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+namespace CompanyClient
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // "BasicHttpBinding_ICompanyPublicService" ifadesi web.configte yazan endpoint namedir
+            CompanyService.CompanyPublicServiceClient client = 
+                new CompanyService.CompanyPublicServiceClient("BasicHttpBinding_ICompanyPublicService");
+            Label1.Text = client.GetPublicInformation();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            // "BasicHttpBinding_ICompanyPublicService" ifadesi web.configte yazan endpoint namedir
+            CompanyService.CompanyConfidentialServiceClient client =
+                new CompanyService.CompanyConfidentialServiceClient("NetTcpBinding_ICompanyConfidentialService");
+            Label2.Text = client.GetConfidentialInformation();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage wcfResponse = client.GetAsync("http://localhost:64858/PayMentService.svc/PayBill/155").Result;
+            HttpContent stream = wcfResponse.Content;
+            var data = stream.ReadAsStringAsync();
+            Label3.Text = data.Result;
+        }
+    }
+}
